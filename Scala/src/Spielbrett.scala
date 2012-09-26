7351ed902eb709c7203279380bf41eaa46ddee13
Eva-Maria Eifler <git@maria-eva.de>
2012-09-26 23:08:57 +0200

    Grundinitialisierung des Spielfeldes Spielbrettanlegung

--- Scala/src/Spielbrett.scala
+++ Scala/src/Spielbrett.scala
@@ -1,3 +1,13 @@
 class Spielbrett {
 
+
+						// Das Spielbrett besteht aus neun Spielfeldern,
+						// die nach class "Spielfeld" definiert sind
+						//die felder sind "x" "y" "z" "a" "b" "c" geordnet
+						// eine Zeile besteht aus 3x "zeile" von Feld
+						// eine Spalte besteht aus 3x "spalte" von Feld
+		
+		val spielbrett = Array (Array(ol:Spielfeld,om:Spielfeld,or:Spielfeld),Array(ml:Spielfeld,mm:Spielfeld,mr:Spielfeld),Array(ul:Spielfeld,um:Spielfeld,ur:Spielfeld))
+		
+		
 }
--- Scala/src/Spielfeld.scala
+++ Scala/src/Spielfeld.scala
@@ -1,7 +1,35 @@
 class Spielfeld  {
 
 	val feld = Array (Array(0,0,0),Array(0,0,0),Array(0,0,0))
- 
-	//def zeile(n:Int):{
 
+						//Eine Zeile sei: 
+						//- alle 3 Werte einer Zeile (n)
+						//- die Zeile sei mit dem Buchstaben "n" frei auswählbar
+						//-  in einem neuen Array "zeile" abgespeichert
+	
+			def zeile (n:Int):Array[Int]={
+				val zeile = new Array[Int](3);
+				zeile (0) = feld (n)(0);
+				zeile (1) = feld (n)(1);
+				zeile (2) = feld (n)(2);
+				return zeile;
+			}
+	
+	
+	
+						//Eine Spalte sei:
+						// - alle 3 Werte einer Spalte (m)
+						// - mit dem Buchstaben "m" frein anwählbar
+						// - in einem neuen Array "spalte" abgespeichert
+						
+			def spalte (m:Int):Array[Int]={
+				val spalte = new Array[Int](3);
+				spalte (0) = feld (0)(m); 
+				spalte (1) = feld (1)(m); 
+				spalte (2) = feld (2)(m);
+				return spalte;
+			}
+			
+			
+	
 }
